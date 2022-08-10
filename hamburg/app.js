7da826baf53469455d98cb4fd899ed3193300d59
Marvis Ijenwa <66011932+IjEmpire@users.noreply.github.com>
2022-08-10 13:07:46 +0200

    main source files from local pc
    orginal design files from local computer. 
    made with html, css, sass and javascript

---
+++ hamburg/app.js
@@ -0,0 +1,13 @@
+//connect dark mode icon to change theme
+
+const page = document.body;
+const darkModeBtn = document.getElementById("btn");
+
+darkModeBtn.addEventListener("click", function(nowDark){
+    page.classList.toggle("dark-mode");
+});
+
+
+brand.addEventListener("click", function(){
+    window.open("https://facebook.com", "_self");
+});
